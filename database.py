4b74d92cffb3120dc11510314bc7f761fdcc31c3
wntjd-0612 <wntjdwntjd612@gmail.com>
2024-05-29 15:51:16 +0900

    fix

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://wntjd:0612@localhost/find_foodtruck"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models, schemas, crud
+import uvicorn
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return {"asdf"}
+@app.post("/api/register", response_model=schemas.Place)
+def register_place(place: schemas.PlaceCreate, db: Session = Depends(get_db)):
+    db_place = crud.create_place(db=db, place=place)
+    return db_place
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/api/data", response_model=list[schemas.Place])
+def get_data(db: Session = Depends(get_db)):
+    places = crud.get_places(db)
+    return places
